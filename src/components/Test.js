59e6a0d04e1173f340a4426eb5b1ce03ddac8c34
Himanshu Kara <kara_himanshu@yahoo.com>
2021-06-09 23:01:33 +0530

    Basic Setup

---
+++ src/components/Test.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const list = {
+  glossary: {
+    title: "example glossary",
+    GlossDiv: {
+      title: "S",
+      GlossList: {
+        GlossEntry: {
+          ID: "SGML",
+          SortAs: "SGML",
+          GlossTerm: "Standard Generalized Markup Language",
+          Acronym: "SGML",
+          Abbrev: "ISO 8879:1986",
+          GlossDef: {
+            para: "A meta-markup language, used to create markup languages such as DocBook.",
+            GlossSeeAlso: ["GML", "XML"],
+          },
+          GlossSee: "markup",
+        },
+      },
+    },
+  },
+};
+
+const Test = (props) => {
+  return <div>
+      <h1>{props.glossary}</h1>
+  </div>;
+};
+
+export default Test;
